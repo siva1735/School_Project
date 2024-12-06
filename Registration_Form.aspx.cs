9d4d561cc043ee8dec696cf49e4bdb712e359ec6
PremPothina <premkumar01860@gmail.com>
2024-12-06 11:57:04 +0530

    Add files via upload

---
+++ Registration_Form.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace School_Project
+{
+    public partial class Registration_Form : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
